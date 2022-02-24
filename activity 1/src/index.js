e416552af9cdb4cb2a904ec15c9fd020104c4972
D4rkst4rr <forgotten692@gmail.com>
2022-02-24 21:51:36 +0800

    first commit

---
+++ activity 1/src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Joshua E. Copian </h1>
+<div>
+  A 3rd year BSIT student of La Salle University. You can visit my Facebook
+  <a href="https://www.facebook.com/jackie.copian" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
